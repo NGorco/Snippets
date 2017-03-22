1d32e7e889fc8fd566e18a39c1f1a27254ce2343
Alexey Petlenko <alexeypetlenko@gmail.com>
2017-03-22 19:05:07 +0200

    Done with intersection logic

--- JS/DragDrop.js
+++ JS/DragDrop.js
@@ -19,13 +19,13 @@
 
         this.handle = options.elem;
         this.elem = options.elem;
-        this.container = options.container || null;
         this.onDragStart = typeof options.onDragStart == 'function' ? options.onDragStart : function() {};
         this.onDrag = typeof options.onDrag == 'function' ? options.onDrag : function() {};
         this.onDragEnd = typeof options.onDragEnd == 'function' ? options.onDragEnd : function() {};
         this.dispatchDragEvents = ( options.dispatchDragEvents || options.useDrop ) || false;
 
         var self = this;
+
         this.selected = false;
 
         if ( options.handler_selector != undefined ) {
@@ -33,22 +33,52 @@
             this.handle = this.elem.querySelector(options.handler_selector);
         }
 
-        var self = this;
-
         var innerOffset = {x: 0, y: 0};
+            //containerOffset = {x: 0, y: 0};
 
         /** Events */
 
         /** Drag start */
         this.handle.addEventListener('mousedown', function(e) {
 
-            self.selected = true;           
-            innerOffset.x = pageC(e, 'x') - ( parseInt(self.elem.style.left) || 0 );
-            innerOffset.y = pageC(e, 'y') - ( parseInt(self.elem.style.top) || 0 );
-
-            self.onDragStart(e);
-
             e.stopPropagation();
+
+            self.selected = true;
+            var offsetParent = {top: 0, left: 0};
+
+            var calc = getComputedStyle(self.elem);
+
+            if ( calc.left == '0px' && calc.position == 'relative' ) {
+
+                offsetParent.left = self.elem.offsetLeft;
+            }
+
+            if ( calc.top == '0px' && calc.position == 'relative' ) {
+
+                offsetParent.top = self.elem.offsetTop;
+            }
+
+            // Get offsets           
+            innerOffset = {
+                x: pageC(e, 'x') - ( parseInt(calc.left) || 0 ) - offsetParent.left,
+                y: pageC(e, 'y') - ( parseInt(calc.top) || 0 ) - offsetParent.top
+            }
+
+            /*var container = self.elem.parentNode.getBoundingClientRect();
+
+            containerOffset = {
+                x: container.left,
+                y: container.top
+            }*/
+
+            var ev = {
+                draggedDOM: self.elem,
+                dragged: self,
+                mouseEv: e
+            }
+
+            self.onDragStart(ev);
+
 
             document.addEventListener('mouseup', mouseUpHandler);
             document.addEventListener('mousemove', mouseMoveHandler);
@@ -67,14 +97,20 @@
             self.elem.style.left = x_pos + 'px';
             self.elem.style.top = y_pos + 'px';
 
-            self.onDrag(e);
+            var ev = {
+                mouseEv: e,
+                dragged: self,
+                draggedDOM: self.elem
+            }
+
+            self.onDrag(ev);
 
             if ( ! self.dispatchDragEvents ) return;
 
             document.dispatchEvent(new CustomEvent('draggableMove', {
             	detail: {
 
-            		draggedItem: self,
+            		dragged: self,
             		draggedDOM: self.elem,
             		mouseEv: e	
             	}
@@ -84,23 +120,30 @@
         /** Drag end */
         function mouseUpHandler(e) {
 
-        	self.selected = false;
-            self.onDragEnd(e);
-
-            if ( ! self.dispatchDragEvents ) return;
-
             e.stopPropagation();
 
-            document.dispatchEvent(new CustomEvent('draggableEnd', {
-            	detail: {
-            		draggedItem: self,
-            		draggedDOM: self.elem,
-            		mouseEv: e
-            	}
-            }));
-            
+            self.selected = false;
+
+            var ev = {
+                mouseEv: e,
+                dragged: self,
+                draggedDOM: self.elem
+            };
+
+            self.onDragEnd(ev);
+
             removeMouseUp();
             document.removeEventListener('mousemove', mouseMoveHandler);
+
+            if ( self.dispatchDragEvents ) {
+
+                document.dispatchEvent(new CustomEvent('draggableEnd', {
+                    detail: ev
+                }));
+
+                // Clear all WillDropIn flags
+                document.dispatchEvent(new CustomEvent('notWillDropIn'));
+            }
         }
 
         function removeMouseUp() { document.removeEventListener('mouseup', mouseUpHandler); }
@@ -134,7 +177,7 @@
     var droppable = function(options) {
 
         this.elem = options.elem;
-        this.dragOver = options.dragOver || function(){};
+        this.onDragOver = options.onDragOver || function(){};
         this.onDrop = options.onDrop || function(){};
         this.onWillDropIn = options.onWillDropIn || function(){};
         this.onNotWillDropIn = options.onNotWillDropIn || function(){};
@@ -143,11 +186,11 @@
         var self = this;
         self.draggedElementOver = false;
 
-        document.addEventListener('draggableMove', function(e) {
+        document.addEventListener('draggableCalcIntersection', function(e) {
 
         	if ( intersect(self.elem, e.detail.draggedDOM) ) {
 
-        		self.dragOver(e);
+        		self.onDragOver(e);
         		self.draggedElementOver = true;
         	} else {
 
@@ -157,7 +200,12 @@
 
         this.elem.addEventListener('WillDropIn', function(e) {
 
-            self.onWillDropIn(e);
+            self.onWillDropIn({
+                dropArea: self,
+                dropAreaDOM: self.elem,
+                dragged: e.detail.dragged,
+                draggedDOM: e.detail.draggedDOM,
+            });
         });
 
 
@@ -171,7 +219,11 @@
 
         document.addEventListener('notWillDropIn', function(e) {
 
-        	self.onNotWillDropIn({dropAreaItem: self, dropAreaDOM: self.elem});
+            self.WillDropIn = false;
+        	self.onNotWillDropIn({
+                dropArea: self,
+                dropAreaDOM: self.elem
+            });
         });
 
         document.addEventListener('draggableEnd', function(e) {
@@ -179,9 +231,9 @@
         	if ( !! self.WillDropIn ) {
 
                 var ev = {
-                    dropAreaItem: self,
+                    dropArea: self,
                     dropAreaDOM: self.elem,
-                    droppedItem: e.detail.draggedItem,
+                    dropped: e.detail.dragged,
                     droppedDOM: e.detail.draggedDOM,
                     mouseEv: e.detail.mouseEv
                 }
@@ -199,32 +251,6 @@
         	self.draggedElementOver = false;
         	self.WillDropIn = false;
         });
-
-        /** Utils */
-        function intersect(DOM1, DOM2) {
-
-        	var obj1 = DOM1.getBoundingClientRect(),
-        		obj2 = DOM2.getBoundingClientRect(),
-        		obj1point1 = {x: obj1.left, y: obj1.top},
-        		obj1point2 = {x: obj1.left + obj1.width, y: obj1.top + obj1.height},
-        		obj2point1 = {x: obj2.left, y: obj2.top},
-        		obj2point2 = {x: obj2.left + obj2.width, y: obj2.top + obj2.height};
-
-
-        	if ( obj1point1.x < obj2point1.x && obj2point1.x < obj1point2.x &&
-        		 obj1point1.y < obj2point1.y && obj2point1.y < obj1point2.y ) return true;
-
-        	if ( obj1point1.x < obj2point2.x && obj2point2.x < obj1point2.x &&
-        		 obj1point1.y < obj2point2.y && obj2point2.y < obj1point2.y ) return true;
-
-        	if ( obj1point1.x < obj2point2.x && obj2point2.x < obj1point2.x &&
-        		 obj1point1.y < obj2point1.y && obj2point1.y < obj1point2.y ) return true;
-
-        	if ( obj1point1.x < obj2point1.x && obj2point1.x < obj1point2.x &&
-        		 obj1point1.y < obj2point2.y && obj2point2.y < obj1point2.y ) return true;
-
-        	return false;
-        }
     }
 
     /** Droppable areas manager
@@ -232,12 +258,23 @@
 	 *  Problem is: if draggable over several elements, where to drop?
 	 *  Solution is droppable Manager
      */
-    document.addEventListener('draggableMove', function() {
+    document.addEventListener('draggableMove', function(e) {
 
     	var areas = [];
 
     	// Clear all WillDropIn flags
     	document.dispatchEvent(new CustomEvent('notWillDropIn'));
+
+
+        // Define, on what elems draggable is over
+        document.dispatchEvent(new CustomEvent('draggableCalcIntersection', {
+            detail: {
+                draggedDOM: e.detail.draggedDOM,
+                dragged: e.detail.dragged, 
+                mouseEv: e
+            }
+        }));
+
     	document.dispatchEvent(new CustomEvent('getDragOverAreas', {
     		detail: {
     			sendArea: function(area) {
@@ -247,7 +284,20 @@
     		}
     	}));
 
-    	areas.forEach(function(item, i) {
+        var areasClone = areas.slice();
+
+        areas.forEach(function(item) {
+
+            areas.forEach(function(secItem) {
+
+                if ( item.elem.parentsObj(secItem.elem) ) {
+
+                    areasClone.splice(areasClone.indexOf(secItem), 1);
+                }
+            });
+        });
+
+    	areasClone.forEach(function(item, i) {
 
     		if ( i == 0 ) {
 
@@ -255,8 +305,8 @@
                 item.elem.dispatchEvent(new CustomEvent('WillDropIn', {
 
                     detail: {
-                        DOMitem: item.elem,
-                        item: item
+                        dropped: item.elem,
+                        droppedDOM: item
                     }
                 }));
     		} else {
@@ -266,6 +316,22 @@
     	});
     });
 
+    /** Utils */
+    function intersect(DOM1, DOM2) {
+
+        var obj1 = DOM1.getBoundingClientRect(),
+            obj2 = DOM2.getBoundingClientRect(),
+            obj1point1 = {x: obj1.left, y: obj1.top},
+            obj1point2 = {x: obj1.left + obj1.width, y: obj1.top + obj1.height},
+            obj2point1 = {x: obj2.left, y: obj2.top},
+            obj2point2 = {x: obj2.left + obj2.width, y: obj2.top + obj2.height};
+
+        var x_overlap = Math.max(0, Math.min(obj1point2.x,obj2point2.x) - Math.max(obj1point1.x,obj2point1.x));
+        var y_overlap = Math.max(0, Math.min(obj1point2.y,obj2point2.y) - Math.max(obj1point1.y,obj2point1.y));
+        var overlapArea = x_overlap * y_overlap;
+
+        return !!overlapArea;
+    }
 
     window.droppable = droppable;
 }();
@@ -315,3 +381,30 @@
         CustomEvent.prototype.EventDataTransfer = MouseEvent.prototype.EventDataTransfer = new EventDataTransfer;
     }
 }();
+
+/**
+ * parentsObj realization in Vanilla JS
+ */
+!(function() {
+
+    if ( ! Element.prototype.parentsObj ) {
+
+        Element.prototype.parentsObj = function(elementToCheck) {
+
+            var isParent = false;
+            var elem = this;
+
+            while ( ! isParent && elem != document.body ) {
+
+                if ( elem.parentNode == elementToCheck ) {
+
+                    isParent = true;
+                }
+
+                elem = elem.parentNode;
+            }
+
+            return isParent;
+        }
+    }
+})();
