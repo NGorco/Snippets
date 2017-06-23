c23a491a56af8394b910fa3b594cb6d60b63a9cb
Alexey Petlenko <alexeypetlenko@gmail.com>
2017-06-23 20:56:32 +0300

    Updated localDB gist
    - Now using Promises

--- JS/DbLocalStorage.js
+++ JS/DbLocalStorage.js
@@ -1,103 +1,95 @@
-var localDB = (function DB (){
+function localDB(args) {
 
-	var instance = {};
 
-	instance.count = 0;
-	instance.id = window.location.hash.substring(1);
-	instance.table_head = 0;
-	instance.DBsize = 5 * 1024 * 1024; // 5 MB
+    if (!args.dbName) {
 
-	/**
-	* Открываем БД
-	*/
-	instance.open = function(clbk)
-	{
-		instance.db = openDatabase("Reestr","1.0", "Reestr data", instance.DBsize);
-		console.log("DB.open()");
-	}
+        throw 'No dbName specified';
+    }
 
-	/**
-	* Выполнение транзакции из массива транзакций
-	*/
-	instance.transactionArr = function(tl, clbk)
-	{
-		console.log("Transaction start");
+    var self = this;
 
-		if(clbk == "undefined") clbk = function(){};
+    open();
 
-		if(tl== undefined  || tl.length < 1) return false;
 
-		instance.db.transaction(function(tx)
-		{
-			var cnt =  tl.length;
-			var check = cnt;
+    /**
+     * Open DB
+     */
+    function open() {
 
-			for(var i = 0; i< cnt;i++)
-			{
-				if(typeof tl[i] == "string")
-				{
-					tx.executeSql(tl[i],[], function(tx)
-					{
-						check--;
+        self.db = openDatabase(
+            args.dbName,
+            args.dbVer || "1.0",
+            args.dbDescr || "",
+            (args.size || 5) 1024 1024
+        );
+    }
 
-						if(check==0)
-						{
-							console.log("Transaction stack is empty, transaction completed");
-							clbk(tx);
-						}
-					},function(tx, error){throw(error)});
-				}else{
 
-					tx.executeSql(tl[i][0],tl[i][1]?tl[i][1]:[], function(tx)
-					{
-						check--;
-						if(check == 0) {
-							console.log("Transaction stack is empty, transaction completed");
-							clbk(tx);
-						}
-					},function(tx, error){throw(error.message)});
-				}
-			}
-		},function(){}, function(tx){});
-	}
 
-	/**
-	* Выполнение запроса к БД
-	*/
-	instance.query = function(query, vars, clbk)
-	{
-		if(!clbk) clbk = function(){};
-		if(vars == undefined) vars = [];
+    /**
+     * Do transaction
+     */
+    this.doTransaction = function(transArr) {
 
-		instance.db.transaction(function(tx)
-		{
-			tx.executeSql(query,vars, function (tx, res)
-			{
-				if(res) clbk(res);
-			},
-			function(tx, error)
-			{
-				throw(error.message);
-			});
-		});
-	}
+        console.log("Transaction start");
 
-	return instance;
-})();
+        if (typeof transArr == 'string') {
+
+            transArr = [
+                [transArr, arguments[1]]
+            ];
+        }
+
+        if (!Array.isArray(transArr)) return false;
+
+        var mainDef = new Promise(function(resolve, reject) {
+
+            self.db.transaction(
+                function(SQLTrans) {
+
+                    transArr.forEach(function(transaction) {
+
+                        SQLTrans.executeSql(
+                            transaction[0],
+                            transaction[1] || []
+                        );
+                    });
+                },
+                function(results, SQLTrans) {
+
+                    resolve(results, SQLTrans);
+                },
+                function(SQLTrans, error) {
+
+                    reject(error, SQLTrans);
+                }
+            );
+        });
+
+        return mainDef;
+    }
+};
 
 /**
  *
  * Examples
 
-  tl.push(
-  ["INSERT INTO properties(id,objid, prop_id, prop_value, add_objid) VALUES (NULL,?, ?, ?,?)",
-	[id, prop_id, prop_value, objid]
+ var tr = new localDB({dbName: 'ty'});
+ var tl = [];
+
+   tl.push(
+    ["INSERT INTO properties(id,objid, prop_id, prop_value, add_objid) VALUES (NULL,?, ?, ?,?)",
+  [id, prop_id, prop_value, objid]
+ ]);
 
 OR
 
-  tl.push("DROP TABLE IF EXISTS properties");
+   tl.push("DROP TABLE IF EXISTS properties");
 
-  instance.transactionArr(tl, function(){alert('ololo')});
-]);
+   tr.doTransaction(tl)
+ .then(function(results){
+    
+ console.log(results)
+ });
 
  * */
